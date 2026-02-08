e78b5124a687b889c2820e889fee159d8a504e46
Gabriele <s349372@studenti.polito.it>
2026-02-08 23:44:26 +0100

    Uncomment production API base URL and comment out localhost URLs in API configuration files

--- frontend/officer_interface/lib/services/CONFIG/api.dart
+++ frontend/officer_interface/lib/services/CONFIG/api.dart
@@ -1,10 +1,10 @@
 class Api {
   
   // PRODUCTION (Railway)
-  //static const String baseUrl = 'https://tps-production-c025.up.railway.app/api';
+  static const String baseUrl = 'https://tps-production-c025.up.railway.app/api';
   
   // LOCALHOST (Android Emulator)
-   static const String baseUrl = 'http://10.0.2.2:8000/api'; 
+  // static const String baseUrl = 'http://10.0.2.2:8000/api'; 
   
   // LOCALHOST (iOS Simulator / Web)
   // static const String baseUrl = 'http://127.0.0.1:8000/api';
